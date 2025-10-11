24990691529db2c52bfaa7e2f96f1fe09fcd968e
Balram Meena <bmeena97562@gmail.com>
2025-10-11 13:17:02 +0530

    forEach, map, filter, reduce, set, Map

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,35 @@
-const date=new Date(Date.now());
+// function createCustomer() {
+//   let balance = 200;
+//   return {
+//     getBalance: function () {
+//       console.log(balance);
+//     },
+//     updateBalance(amount) {
+//       if (typeof amount !== "number" || amount === 0)
+//         throw new Error("amount not valid");
+//       balance += amount;
+//       console.log("Available Balance: ",balance)
+//     },
+//   };
+// }
 
-console.log(date)
+// const customer=createCustomer();
+// try{
+//     customer.updateBalance(34);
+// }catch(err){
+//     console.log(err.message
+//     )
+// }
+
+// function fun(value) {
+//   return function temp(num) {
+//     console.log(value * num);
+//   }
+// }
+
+// fun(10)(5);
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const arr1=arr.map(e=>(e/2).toFixed(3));
+console.log(arr1)
